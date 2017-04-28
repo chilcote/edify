1467ad1d4ae6a1474fb5222dbc6e31fc2c6b50ae
Joseph Chilcote <chilcote+github@gmail.com>
2017-04-28 13:52:06 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''setup for edify'''
+
+from distutils.core import setup
+import setuptools
+
+setup(name='edify',
+      version='0.1.0',
+      description='This script displays information on commonly run commands.',
+      author='Joseph Chilcote',
+      author_email='chilcote@gmail.com',
+      url='https://www.github.com/chilcote/edify/',
+      license='Apache 2.0',
+      packages=['edify'],
+     )
